f4a23bea4bb171076678b031668e47e09abbc33c
AndriiMoskalenko <andreymus0@gmail.com>
2023-02-17 12:44:51 +0200

    update: clear Image Update

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -2,17 +2,14 @@ import { useState, useEffect } from "react"
 
 const ImageUpload = ({ imageHandler }) => {
   const [selectedFile, setSelectedFile] = useState()
-  // const [preview, setPreview] = useState()
 
-  // create a preview as a side effect, whenever selected file is changed
+  // side effect, whenever selected file is changed
   useEffect(() => {
     if (!selectedFile) {
-      // setPreview(undefined)
       return
     }
 
     const objectUrl = URL.createObjectURL(selectedFile)
-    // setPreview(objectUrl)
 
     imageHandler(selectedFile)
 
@@ -26,16 +23,14 @@ const ImageUpload = ({ imageHandler }) => {
       return
     }
 
-    // I've kept this example simple by using the first image instead of multiple
+    // first image instead of multiple
     setSelectedFile(e.target.files[0])
   }
 
   return (
     <div>
       <input type="file" onChange={onSelectFile} />
-      {/* {selectedFile && (
-        <img src={preview} style={{ width: "200px", height: "200px" }} />
-      )} */}
+
     </div>
   )
 }
