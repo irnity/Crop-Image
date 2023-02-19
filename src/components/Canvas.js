c6bb14b74c82e901d3ca32ea84abefde7e568e97
AndriiMoskalenko <andreymus0@gmail.com>
2023-02-19 14:29:40 +0200

    update: mask crop

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react"
-import background from "../images/one.jpg"
 import classes from "./Canvas.module.css"
+import mask from "../images/faceMask.svg"
 
 function Canvas({ loadedImage }) {
   // conect ref to canvas
@@ -11,22 +11,17 @@ function Canvas({ loadedImage }) {
   const [size, setSize] = useState(200)
   const [angle, setAngle] = useState(0)
 
-  //
-  const [isDrawing, setIsDrawing] = useState(false)
-
   // move direction
-  const [X, setX] = useState(0)
-  const [olxX, setOlxX] = useState(0)
+  const [isMouseDragging, setIsMouseDragging] = useState(false)
+  const [xPosition, setXPosition] = useState(0)
+  const [olxX, setPreviousX] = useState(0)
+  const [yPosition, setYPosition] = useState(0)
+  const [olxY, setPreviousY] = useState(0)
 
-  const [Y, setY] = useState(0)
-  const [olxY, setOlxY] = useState(0)
+  const [downloaded, setDownloaded] = useState(false)
 
   // download image
   const image = useMemo(() => new Image(), [])
-  image.src = URL.createObjectURL(loadedImage)
-
-  const backImage = useMemo(() => new Image(), [])
-  backImage.src = background
 
   // start Render
   useEffect(() => {
@@ -37,73 +32,58 @@ function Canvas({ loadedImage }) {
     canvas.height = 600
 
     ctxRef.current = ctx
-    setX(ctxRef.current.canvas.width / 2.5)
-    setY(ctxRef.current.canvas.height / 2.5)
-    draw()
+    setXPosition(canvasRef.current.width / 2.5)
+    setYPosition(canvasRef.current.height / 2.5)
   }, [])
 
   // rendering image
   const draw = useMemo(
     () =>
       function draw() {
+        image.src = URL.createObjectURL(loadedImage)
         image.onload = () => {
-          // if we need background image
-
-          // ctxRef.current.drawImage(
-          //   backImage,
-          //   0,
-          //   0,
-          //   ctxRef.current.canvas.width,
-          //   ctxRef.current.canvas.height
-          // )
-
           // clear canvas
           ctxRef.current.clearRect(
             0,
             0,
-            ctxRef.current.canvas.width,
-            ctxRef.current.canvas.height
+            canvasRef.current.width,
+            canvasRef.current.height
           )
 
           // make crop
-
-          let circlePath = new Path2D()
-
-          circlePath.ellipse(
-            ctxRef.current.canvas.width / 2,
-            ctxRef.current.canvas.height / 2,
-            ctxRef.current.canvas.width / 3.5,
-            ctxRef.current.canvas.height / 3,
-            0,
-            0,
-            Math.PI * 2
-          )
-
-          // Set the clip to the circle
-          ctxRef.current.clip(circlePath)
-          // Set the clip to be the intersection of the circle and the square
-          // ctxRef.current.clip(squarePath)
+          if (downloaded === true) {
+            ctxRef.current.save()
+            let Mask = new Path2D(
+              "M2.3699 88.9367C-2.11311 42.3243 34.7252 2 81.7913 2H90.0318C137.296 2 174.198 42.6479 169.404 89.4275L159.328 187.749C155.472 225.378 123.62 254 85.6014 254C47.4065 254 15.4674 225.12 11.8294 187.293L2.3699 88.9367Z "
+            )
+            const position = new Path2D()
+            position.addPath(Mask, {
+              e: canvasRef.current.width / 2.8,
+              f: canvasRef.current.height / 3.5,
+            })
+            ctxRef.current.clip(position)
+          }
 
           ctxRef.current.save()
 
           ctxRef.current.translate(
-            ctxRef.current.canvas.width / 2,
-            ctxRef.current.canvas.height / 2
+            canvasRef.current.width / 2,
+            canvasRef.current.height / 2
           )
           ctxRef.current.rotate((Math.PI / 180) * angle)
 
           ctxRef.current.translate(
-            -(ctxRef.current.canvas.width / 2),
-            -(ctxRef.current.canvas.height / 2)
+            -(canvasRef.current.width / 2),
+            -(canvasRef.current.height / 2)
           )
 
           // draw main image that we insert
-          ctxRef.current.drawImage(image, X, Y, size, size)
+          ctxRef.current.drawImage(image, xPosition, yPosition, size, size)
 
           ctxRef.current.restore()
         }
       },
-    [X, Y, angle, image, size]
+    [image, loadedImage, downloaded, angle, xPosition, yPosition, size]
   )
 
   // main render when we move image
@@ -113,54 +93,57 @@ function Canvas({ loadedImage }) {
 
   // change state of moving
   const startDrawing = () => {
-    setIsDrawing(true)
+    setIsMouseDragging(true)
   }
 
   // move our image
   const move = ({ nativeEvent }) => {
-    if (!isDrawing) {
+    if (!isMouseDragging) {
       return
     }
     const { offsetX, offsetY } = nativeEvent
 
     // right
     if (offsetX > olxX) {
-      setX((prevState) => prevState + 4)
-      setOlxX(offsetX)
+      setXPosition((prevState) => prevState + 4)
+      setPreviousX(offsetX)
       // left
     } else if (offsetX < olxX) {
-      setX((prevState) => prevState - 4)
-      setOlxX(offsetX)
+      setXPosition((prevState) => prevState - 4)
+      setPreviousX(offsetX)
     }
 
     // up
     if (offsetY > olxY) {
-      setY((prevState) => prevState + 4)
-      setOlxY(offsetY)
+      setYPosition((prevState) => prevState + 4)
+      setPreviousY(offsetY)
       // down
     } else if (offsetY < olxY) {
-      setY((prevState) => prevState - 4)
-      setOlxY(offsetY)
+      setYPosition((prevState) => prevState - 4)
+      setPreviousY(offsetY)
     }
   }
 
   // change state of moving
   const finishDrawing = () => {
-    setIsDrawing(false)
+    setIsMouseDragging(false)
   }
 
   // change size of image
   const sizeHandler = (e) => {
-    setSize((prevState) => (prevState = e.target.value))
+    setSize(e.target.value)
   }
 
   // change angle of image if we change it move direction is changed to
   // that will make move top change to move left for example
   const angleHandler = (e) => {
     setAngle(e.target.value)
-    draw()
-    setX(ctxRef.current.canvas.width / 4)
-    setY(ctxRef.current.canvas.height / 4)
+    setXPosition(canvasRef.current.width / 4)
+    setYPosition(canvasRef.current.height / 4)
+  }
+
+  const toggleCropHandler = () => {
+    setDownloaded(true)
   }
 
   // save image canvas
@@ -173,6 +156,9 @@ function Canvas({ loadedImage }) {
 
   return (
     <div className={classes.box}>
+      <div className={classes.mask_box}>
+        <img src={mask} alt="d" className={classes.mask} />
+      </div>
       <div className={classes.canvasBox}>
         <canvas
           className={classes.canvas}
@@ -216,9 +202,26 @@ function Canvas({ loadedImage }) {
             onChange={angleHandler}
           />
         </div>
+        <div className={classes.setting}>
+          <label className={classes.setting_input} htmlFor="crop">
+            Crop
+          </label>
+          <input
+            className={classes.setting_input}
+            type="button"
+            id="crop"
+            name="crop"
+            onClick={toggleCropHandler}
+            value={downloaded}
+          />
+        </div>
 
         <div className={classes.setting}>
-          <a className={classes.download} href="download_link" onClick={saveImageToLocal}>
+          <a
+            className={classes.download}
+            href="download_link"
+            onClick={saveImageToLocal}
+          >
             Download Image
           </a>
         </div>
